96b4d40b0d9a8b1077d7742c00ed2edeaa9962a2
Geun Hee Han <49366446+geunhee725@users.noreply.github.com>
2024-11-15 16:32:16 +0900

    Update clustering_prot_phos.m

--- clustering_prot_phos.m
+++ clustering_prot_phos.m
@@ -1,23 +1,23 @@
 %% 1. normalization of protein/phosphopeptide data for clustering analysis
-% import raw abundance data as 'allo_raw'
+% 1) import raw abundance data as 'allo_raw'
 allo_raw(allo_raw==0)=NaN;
 
-% normalized between TMT channels
+% 2) normalized between TMT channels
 allo_ctl=allo_raw(:,1:18);
 allo_tx=allo_raw(:,19:36);
 
 tm_ctl_q=quantilenorm(allo_ctl);
 tm_tx_q=quantilenorm(allo_tx);
 
-% log2-transformation
+% 3) log2-transformation
 tm_log_ctl=log2(tm_ctl_q+1);
 tm_log_tx=log2(tm_tx_q+1);
 
-% fold-change calculation relative to global reference
+% 4) fold-change calculation relative to global reference
 tm_ratio_ctl=tm_log_ctl-repmat(tm_log_ctl(:,1),1,18);tm_ratio_ctl=tm_ratio_ctl(:,2:end); 
 tm_ratio_tx=tm_log_tx-repmat(tm_log_tx(:,1),1,18);tm_ratio_tx=tm_ratio_tx(:,2:end);
 
-% normalize between TMT sets
+% 5) normalize between TMT sets
 tm_rs_ctl=reshape(tm_ratio_ctl,[size(tm_ctl_q,1)*17,1]);
 tm_rs_tx=reshape(tm_ratio_tx,[size(tm_ctl_q,1)*17,1]);
 
@@ -32,6 +32,26 @@ FC_q=quantilenorm(FC);
 
 tm_ind_exp=sum(isnan(FC_q),2)==0;
 FC_q=FC_q(tm_ind_exp,:);
+
+%% 3. ONMF clustering
+% 1) Filter Top MAD protein/phosphopeptides
+FC_q_mad=mad(FC_q,1,2);
+ind_mad{1,1}=find(FC_q_mad>prctile(FC_q_mad,90));
+ind_mad{2,1}=find(FC_q_mad>prctile(FC_q_mad,80));
+ind_mad{3,1}=find(FC_q_mad>prctile(FC_q_mad,70));
+
+% 2) ONMF clustering
+for i=1:3
+    i
+    tm_data=FC_q(ind_mad{i,1},:);
+    for j=2:8
+        j
+        [coph_cor{i}(j-1),ave_C{i}{j-1},~,~]=aoNMF_subtyping(auto(tm_data'),j,1000,100);
+    end
+end
+
+% 3) Cluster consensus value matrix
+CG_mad30_c2=clustergram(ave_C{1,3}{1,1},'cluster',3,'standardize',3,'Linkage','complete');
 
 
 
